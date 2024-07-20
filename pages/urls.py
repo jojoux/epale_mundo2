3e0f6a73793805809040a4544641d34cf5aee978
jojoux <ritzraton1@gmail.com>
2024-07-20 11:49:52 -0400

    about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from pages.views import HomeView
+from pages.views import HomeView, AboutPageview
+
 urlpatterns = [
-    path("electronica",HomeView.as_view(),name='home')
+    path("electronica",HomeView.as_view(),name= "home"),
+    path("about/", AboutPageview.as_view(), name= "about"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name= "home.html"
-    
+
+class AboutPageview(TemplateView):
+        template_name = "about.html"
